2ba9567066e6a322636cc644b3f69264b2da367b
Tawan <arthur@avatolabs.com>
2025-11-13 00:26:00 +0800

    Fix migration file: keep kaspad_version reference for historical accuracy

--- processing/database/migrations/000008_rename_kaspad_to_tondid.up.sql
+++ processing/database/migrations/000008_rename_kaspad_to_tondid.up.sql
@@ -1,3 +1,3 @@
--- Rename Tondid_version column to tondid_version in app_config table
-ALTER TABLE app_config RENAME COLUMN Tondid_version TO tondid_version;
+-- Rename kaspad_version column to tondid_version in app_config table
+ALTER TABLE app_config RENAME COLUMN kaspad_version TO tondid_version;
 
